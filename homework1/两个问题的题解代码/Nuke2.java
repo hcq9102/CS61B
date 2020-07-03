5807531afab6eb28a923d5f83cdab07e0ce14299
hcq9102 <49005493+hcq9102@users.noreply.github.com>
2020-07-02 21:11:57 -0500

    Update Nuke2.java

--- homework1/两个问题的题解代码/Nuke2.java
+++ homework1/两个问题的题解代码/Nuke2.java
@@ -5,7 +5,7 @@ class Nuke2{
 	public static void main(String[] args) 
 	{
 		Scanner sc = new Scanner(System.in);
-		String name = sc.nextLine();//read string input
+		String name = sc.nextLine();//read string type input
 		int n = name.length();
 		String output = name.substring(0,1) + name.substring(2,n);
 		System.out.println(output);
